d765aa15d224c0523cd9cb699f93e3ffc3383eaf
Kensal Ramos <kensalramos@gmail.com>
2020-08-11 14:46:08 -0400

    Update MenuActivity.java - Welcome User
    Added functionality for file reading to the welcome user string.

--- main/java/com/example/fitnesstracker/MenuActivity.java
+++ main/java/com/example/fitnesstracker/MenuActivity.java
@@ -1,10 +1,29 @@
 package com.example.fitnesstracker;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import java.io.File;
+import java.util.Scanner;
+import static com.example.fitnesstracker.CreateAccountActivity.FILE_NAME;
+
+/*
+ * Created/Updated by: Kensal J Ramos
+ *
+ * Name: MenuActivity
+ *
+ * Description: This is the main menu activity. Links to Begin Workout, Progress, Motivation,
+ * and Your Account are included here. Additionally, a "Welcome <username>" string is located
+ * at the bottom.
+ *
+ * As expected, every TextView has an OnClick action that leads to the corresponding activity.
+ *
+ *
+ *
+ */
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -13,6 +32,8 @@ public class MenuActivity extends AppCompatActivity {
         // Work our way up sending in the same Bundle
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_activity);
+
+        setWelcomeString(this);
     }
 
     /*
@@ -50,9 +71,40 @@ public class MenuActivity extends AppCompatActivity {
      */
     public void yourAccOnClick(View view) {
 
-        Intent intent = new Intent(this, YourAccountActivity.class);
+        // Change class
+        Intent intent = new Intent(this, CreateAccountActivity.class);
         startActivity(intent);
 
+    }
+
+    /*
+     * Name: setWelcomeString
+     */
+    private void setWelcomeString(Context context) {
+
+        Scanner scanner;
+        String filePath = context.getFilesDir().getPath().toString() + "/" + FILE_NAME;
+        TextView welcomeText = (TextView) findViewById(R.id.welcomeUserText);
+        String welcomeUser = "";
+
+        // Attempt to create scanner...
+        try {
+            scanner = new Scanner(new File(filePath));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // Take the first line of the file
+        if (scanner.hasNext()) {
+            welcomeUser = getString(R.string.welcome_user, scanner.nextLine());
+        }
+        else {
+            welcomeUser = "Welcome User";
+        }
+
+
+        welcomeText.setText(welcomeUser);
     }
 
 }
