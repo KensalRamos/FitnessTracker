748c4ce56109c5a4ab75bf670b55eab09b085434
Kensal Ramos <kensalramos@gmail.com>
2020-08-11 14:24:32 -0400

    Update MainActivity.java
    Cleaned up some unnecessary imports and added functionality for a file. Please see file for more details.

--- main/java/com/example/fitnesstracker/MainActivity.java
+++ main/java/com/example/fitnesstracker/MainActivity.java
@@ -3,12 +3,9 @@ package com.example.fitnesstracker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import java.io.File;
 
 /*
  *
@@ -16,11 +13,8 @@ import androidx.constraintlayout.widget.ConstraintLayout;
  *
  * Created on 08/03/2020 (MM/DD/YYYY)
  *
- * Description: This will be the main activity screen. Towards the end I would like this
- * activity screen to either log users into their accounts, or have them input new information.
- *
- * The sending of emails, etc will be dealt with much, much later. Initially I just want to get
- * the user's Name, DOB, Gender, Weight (optional) , Height(optional).
+ * Description: This is the welcome screen that is displayed only once per instance.
+ * Nothing much here besides an image and a TextView.
  *
  */
 
@@ -32,14 +26,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+        // Read from text file to try and initialize user account
+        // If unsuccessful, we will initiate the createAccountActivity instead of the MenuActivity
+
         LinearLayout linearLayout = (LinearLayout) findViewById(R.id.mainLinearLayout);
+
         linearLayout.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                Intent intent;
+                String filePath = MainActivity.this.getFilesDir().getPath().toString() + "/" + CreateAccountActivity.FILE_NAME;
+                File file = new File(filePath);
+
+                // if (userInfo.txt is not empty) open menu activity
+                // else open create account activity
+                if (file.exists() && file.length() != 0) {
+                    intent = new Intent(MainActivity.this, MenuActivity.class);
+                }
+                else {
+                    intent = new Intent(MainActivity.this, CreateAccountActivity.class);
+                }
                 startActivity(intent);
+                finishAffinity();
             }
         });
 
